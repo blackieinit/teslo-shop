20dad945baa772a1d0b2f18519db5851b4c4bbd1
Daniel Parra <danielparra@encodingtech.com>
2022-10-04 21:37:34 -0400

    [ADD] Upload files to the backend

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Query, UploadedFile, UseInterceptors} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Post('products')
+  @UseInterceptors( FileInterceptor('image') )
+  uploadImageProduct( 
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    return image;
+  }
 
 }
