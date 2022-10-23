fe9d712fcb2498a8718edbb1f7839e1313af24d2
Daniel Parra <danielparra@encodingtech.com>
2022-10-23 09:03:16 -0400

    [ADD] Login controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -8,6 +9,10 @@ export class AuthController {
   @Post('signup')
   create(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
+  }
+  @Post('login')
+  login(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login( loginUserDto );
   }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,18 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
-
+//import { JwtService } from '@nestjs/jwt';
+import { LoginUserDto } from './dto/login-user.dto';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
 
   constructor(
     @InjectRepository( User )
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+    //private jwtService: JwtService
   ){}
 
   async create(createUserDto: CreateUserDto) {
@@ -22,6 +25,16 @@ export class AuthService {
     } catch (error) {
       this.handleExceptions( error );
     }
+  }
+
+  async login( loginUserDto: LoginUserDto  ) {
+    const { email, password } = loginUserDto;
+    const user = await this.userRepository.findOneBy( {email} );
+    
+    if ( !user || !await bcrypt.compare(password, user.password)) 
+      throw new NotFoundException(`User not found`);
+
+    return user
   }
 
   private handleExceptions( error: any ) {
